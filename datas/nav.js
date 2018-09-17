3a4cb0c1164aa2323ecb4b18fb5b473d2c5cc519
guiyuhao111 <1060455912@qq.com>
2018-09-17 17:52:41 +0800

    第一次提交

---
+++ datas/nav.js
@@ -0,0 +1,55 @@
+var navs = [{
+	"title": "首页",
+	"icon": "fa-home fa-2x",
+	"spread": true,
+
+}, {
+	"title": "委员管理",
+	"icon": "fa-podcast fa-2x",
+	"spread": false,
+
+}, {
+	"title": "履职管理",
+	"icon": "fa-users fa-2x",
+	"spread": false,
+	"children": [{
+	"title": "调研管理",
+	"icon": "fa-circle-o fa-lg;",
+	"href": "某某.html"
+}, {
+	"title": "视察管理",
+	"icon": "fa-circle-o fa-lg;",
+	"href": "某某.html"
+}, {
+	"title": "活动管理",
+	"icon": "fa-circle-o fa-lg;",
+	"href": "table.html"
+}, {
+	"title": "信息管理",
+	"icon": "fa-circle-o fa-lg;",
+	"href": "nav.html"
+}, {
+	"title": "大会发言",
+	"icon": "fa-circle-o fa-lg;",
+	"href": "tab.html"
+}, {
+	"title": "社情民意",
+	"icon": "fa-circle-o fa-lg;",
+	"href": "auxiliar.html"
+},{
+	"title": "监督管理",
+	"icon": "fa-circle-o fa-lg;",
+	"href": "auxiliar.html"
+},]
+
+}, {
+	"title": "提案管理",
+	"icon": "fa-vcard fa-2x",
+	"href": "",
+	"spread": false
+},{
+	"title": "设置",
+	"icon": "fa-cog fa-2x",
+	"href": "",
+	"spread": false
+}];
